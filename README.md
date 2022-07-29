ca0bc37b54f5f1a6d59d0b2ca6431c8b7eb437ac
Craig Stephenson <crstephenson@alaska.edu>
2022-07-29 15:13:24 -0800

    Add script and input files to generate SERDP FMO files.

---
+++ README.md
@@ -0,0 +1,39 @@
+# ALFRESCO FMO
+
+This repository contains scripts and input files needed to generate the fire management options (FMO) GeoTIFFs that are used as inputs for ALFRESCO simulations. FMO inputs can differ between projects, so each project gets its own subdirectory in this repo.
+
+## Setup
+
+Install the pipenv dependencies:
+
+```
+pipenv install
+```
+
+## Running
+
+To generate FMO files for a particular project, first check the comments in the project's `generate.sh` script to see if any files need to be downloaded or if variables need to be set to local paths. Then run the script like this:
+
+```
+pipenv run ./generate.sh
+```
+
+Outputs will appear in the `output` subdirectory. These outputs should consist of a set of `Ignition*.tif` and `Sensitivity*.tif` files for each FMO scenario. For example:
+
+```
+$ ls output/FMO
+Ignition.tif		Ignition3.tif		Sensitivity2.tif
+Ignition2.tif		Sensitivity.tif		Sensitivity3.tif
+```
+
+Each file in the `Ignition*.tif` and `Sensitivity*.tif` series corresponds to a fire transition year in the ALFRESCO configuration. The file sequence generally corresponds to these transition years:
+
+| FMO Files                           | Fire Transition Year |
+| ----------------------------------- | -------------------- |
+| `Ignition1.tif`, `Sensitivity1.tif` | 1000                 |
+| `Ignition2.tif`, `Sensitivity2.tif` | 1950                 |
+| `Ignition3.tif`, `Sensitivity3.tif` | Present Year         |
+
+Currently, all `Ignition*.tif` files are identical by design. This may change in the future. `Sensitivity2.tif` and `Sensitivity3.tif` may be the same, as well, depending on whether changes to the past vs. future FMO scenarios are being simulated.
+
+Once the GeoTIFFs are generated, include them in the `Spatial.IgnitionFactor` and `Spatial.Sensitivity` sections of the JSON configuration for an ALFRESCO run, as seen in the [example JSON configuration](https://github.com/ua-snap/alfresco/blob/main/examples/alfresco.json).
---
+++ serdp/generate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Set to the extracted ZIP of the AICC management options file geodatabase.
+# Tested with AlaskaWildlandFire_IA_Management_Options_2022.zip
+# Download current version from here: https://fire.ak.blm.gov/predsvcs/maps.php
+AICC_FMO_GDB='input/Alaska_IA_FireManagementOptions.gdb'
+
+mkdir -p tmp output/NoFMO output/FMO output/AltFMO
+
+# Separate and rasterize Critical level polygons
+ogr2ogr -t_srs EPSG:3338 -sql "SELECT * FROM fire_management_options WHERE PROT = 'C'" tmp/reprojected.shp $AICC_FMO_GDB
+gdal_rasterize tmp/reprojected.shp tmp/Alaska_IA_FireManagementOptions_C.tif -a_nodata 0 -a_srs EPSG:3338 -burn 210 -ts 5528 2223 -te -1725223.205807 321412.933 3802776.794 2544412.932644 -ot Float32
+rm tmp/reprojected.*
+
+# Separate and rasterize Full level polygons
+ogr2ogr -t_srs EPSG:3338 -sql "SELECT * FROM fire_management_options WHERE PROT = 'F'" tmp/reprojected.shp $AICC_FMO_GDB
+gdal_rasterize tmp/reprojected.shp tmp/Alaska_IA_FireManagementOptions_F.tif -a_nodata 0 -a_srs EPSG:3338 -burn 235 -ts 5528 2223 -te -1725223.205807 321412.933 3802776.794 2544412.932644 -ot Float32
+rm tmp/reprojected.*
+
+# Separate and rasterize Modified level polygons
+ogr2ogr -t_srs EPSG:3338 -sql "SELECT * FROM fire_management_options WHERE PROT = 'M'" tmp/reprojected.shp $AICC_FMO_GDB
+gdal_rasterize tmp/reprojected.shp tmp/Alaska_IA_FireManagementOptions_M.tif -a_nodata 0 -a_srs EPSG:3338 -burn 260 -ts 5528 2223 -te -1725223.205807 321412.933 3802776.794 2544412.932644 -ot Float32
+rm tmp/reprojected.*
+
+# Create NoFMO rasters
+cp input/extent_mask.tif output/NoFMO/Ignition1.tif
+cp input/extent_mask.tif output/NoFMO/Ignition2.tif
+cp input/extent_mask.tif output/NoFMO/Ignition3.tif
+cp input/extent_mask.tif output/NoFMO/Sensitivity1.tif
+cp input/extent_mask.tif output/NoFMO/Sensitivity2.tif
+cp input/extent_mask.tif output/NoFMO/Sensitivity3.tif
+
+# Create FMO rasters
+cp input/extent_mask.tif output/FMO/Ignition1.tif
+cp input/extent_mask.tif output/FMO/Ignition2.tif
+cp input/extent_mask.tif output/FMO/Ignition3.tif
+cp input/extent_mask.tif output/FMO/Sensitivity1.tif
+pipenv run gdal_merge.py -ot Float32 -of GTiff -o output/FMO/Sensitivity2.tif input/extent_mask.tif tmp/Alaska_IA_FireManagementOptions_C.tif tmp/Alaska_IA_FireManagementOptions_F.tif tmp/Alaska_IA_FireManagementOptions_M.tif
+cp output/FMO/Sensitivity2.tif output/FMO/Sensitivity3.tif
+
+# Create AltFMO rasters
+cp input/extent_mask.tif output/AltFMO/Ignition1.tif
+cp input/extent_mask.tif output/AltFMO/Ignition2.tif
+cp input/extent_mask.tif output/AltFMO/Ignition3.tif
+cp input/extent_mask.tif output/AltFMO/Sensitivity1.tif
+pipenv run gdal_merge.py -ot Float32 -of GTiff -o output/AltFMO/Sensitivity2.tif input/extent_mask.tif tmp/Alaska_IA_FireManagementOptions_C.tif tmp/Alaska_IA_FireManagementOptions_F.tif tmp/Alaska_IA_FireManagementOptions_M.tif
+pipenv run gdal_merge.py -ot Float32 -of GTiff -o output/AltFMO/Sensitivity3.tif input/extent_mask.tif tmp/Alaska_IA_FireManagementOptions_C.tif tmp/Alaska_IA_FireManagementOptions_F.tif input/dod_alt_fmo_areas.tif tmp/Alaska_IA_FireManagementOptions_M.tif
